79d2d2a6770fa1a86ebca2b8a1d44ca1cb24a3dd
khoa lnt <khoa201191@gmail.com>
2022-06-17 00:57:38 +0700

    Bài tập JS_B5

--- method/method.js
+++ method/method.js
@@ -1,6 +1,6 @@
 /**
  * Hàm nhận vào id, trả về DOM id
- * @param {*} id
+ * @param {*} id id là thẻ input cần DOM
  * @returns
  */
 function domID(id) {
@@ -8,23 +8,37 @@ function domID(id) {
 }
 //BÀI 1
 /**
- * Hàm nhận vào các thẻ input, xét kết quả và in ra màn hình
- * @param {*} id1 id1 là thẻ input diemMonThu1
- * @param {*} id2 id2 là thẻ input diemMonThu1
- * @param {*} id3 id3 là thẻ input diemMonThu1
- * @param {*} id4 id4 là thẻ input diemChuan
- * @param {*} id5 id5 là thẻ input khuVuc
- * @param {*} id6 id6 là thẻ input doiTuong
+ * Hàm nhận vào các thẻ input, trả về kết quả điểm trung bình
+ * @param {*} id1 id1 là thẻ input điểm thứ 1
+ * @param {*} id2 id2 là thẻ input điểm thứ 2
+ * @param {*} id3 id3 là thẻ input điểm thứ 3
+ * @returns trả kết quả điểm trung bình
  */
-function xetKetQua(id1, id2, id3, id4, id5, id6) {
+function tinhDiemTB(id1, id2, id3) {
   var diem1 = Number(domID(id1).value);
   var diem2 = Number(domID(id2).value);
   var diem3 = Number(domID(id3).value);
   var diemTB = 0;
   diemTB = diem1 + diem2 + diem3;
+  return diemTB;
+}
+/**
+ * Hàm nhận vào các thẻ input, xét kết quả và in ra màn hình
+ * @param {*} id1 id1 là thẻ input diemMonThu1
+ * @param {*} id2 id2 là thẻ input diemMonThu2
+ * @param {*} id3 id3 là thẻ input diemMonThu3
+ * @param {*} id4 id4 là thẻ input diemChuan
+ * @param {*} id5 id4 là thẻ input khuVuc
+ * @param {*} id6 id4 là thẻ input doiTuong
+ */
+function xetKetQua(id1, id2, id3, id4, id5, id6) {
+  var diem1 = Number(domID(id1).value);
+  var diem2 = Number(domID(id2).value);
+  var diem3 = Number(domID(id3).value);
   var diemChuan = Number(domID(id4).value);
   var khuVuc = domID(id5).value;
   var doiTuong = domID(id6).value;
+  var diemTB = tinhDiemTB("diemMonThu1", "diemMonThu2", "diemMonThu3");
   var ketQua = "";
   if (diem1 === 0 || diem2 === 0 || diem3 === 0) {
     ketQua = "Bạn đã rớt. Do có điểm nhỏ hơn hoặc bằng 0";
